ef6e624642be82ab43a99789c6db3d10628b9a92
memchenko <mikhail.emchenko@gmail.com>
2019-03-11 23:12:57 +0400

    feat: added module ConfigFormatter and few errors

---
+++ src/constants/errors.js
@@ -0,0 +1,3 @@
+export const NO_X_AXIS = 'NO_X_AXIS';
+export const INVALID_COLUMN = 'INVALID_COLUMN';
+export const INVALID_CONFIG = 'INVALID_CONFIG';
---
+++ src/js/modules/ConfigFormatter.js
@@ -0,0 +1,92 @@
+import { LINE, X } from '_constants/chart-types';
+import { NO_X_AXIS, INVALID_COLUMN, INVALID_CONFIG } from '_constants/errors';
+
+import { extractDateInfoFromMS } from '_js/utils/formatTime';
+
+export default class ConfigFormatter {
+    _config = null;
+    _axisValues = null;
+    _graphs = null;
+
+    constructor(data) {
+        this._config = data;
+        const xAxisName = this._safelyGetXAxis(data.types);
+        this._axisValues = this._safelyGetAxisValues(xAxisName, data.columns);
+        this._graphs = this._getGraphs(data);
+
+        if (!this._isGraphsValid(this.graphs)) throw new Error(INVALID_CONFIG);
+    }
+
+    _safelyGetXAxis(types) {
+        const xAxisName = this.getXAxis(types);
+
+        if (xAxisName === null) throw new Error(NO_X_AXIS);
+
+        return xAxisName;
+    }
+
+    _getXAxis(types = []) {
+        const entry = Object.entries(types)
+            .find(entry => entry.type === X);
+
+        return entry === undefined ? null : entry[0];
+    }
+
+    _safelyGetAxisValues(axisName, columns) {
+        const axisValues = this.getAxisValues(axisName, columns);
+
+        if (axisValues === undefined) throw new Error(INVALID_COLUMN);
+
+        return axisValues.slice(1).map((val) => {
+            const { day, month } = extractDateInfoFromMS(val);
+            return `${month} ${day}`;
+        });
+    }
+
+    _getAxisValues(axisName, columns) {
+        return columns.find(graph => graph[0] === axisName);
+    }
+
+    _getGraphs({ colors, names, types, columns }) {
+        return Object.entries(names)
+            .map(([key, name]) => {
+                const values = columns.find(column => column[0] === key).slice(1);
+                return {
+                    name, values, isVisible: true,
+                    color: colors[key], type: types[key],
+                    max: Math.max.apply(null, values),
+                    min: Math.min.apply(null, values)
+                };
+            });
+    }
+
+    _isGraphsValid(graphs) {
+        return graphs.every(({ values, color, type, max, min }) => {
+            return Array.isArray(values) &&
+                !Number.isNaN(min) && !Number.isNaN(max) &&
+                this.isValidColor(color) && this.isValidType(type);
+        });
+    }
+
+    _isValidColor(color) {
+        return String(color)[0] === '#' && String(color).length === 7;
+    }
+
+    _isValidType(type) {
+        return type === X || type === LINE;
+    }
+
+    get axisValues() {
+        return this._axisValues;
+    }
+
+    get graphs() {
+        return this._graphs;
+    }
+
+    toggleGraphVisibility(graphIdx) {
+        if (graphIdx > this._graphs.length || graphIdx < 0) return;
+
+        this._graphs[graphIdx].isVisible = !this._graphs[graphIdx].isVisible;
+    }
+}
